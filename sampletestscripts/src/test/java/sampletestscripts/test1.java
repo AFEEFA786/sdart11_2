03657486dfac134fde29297db92f4f07af61b404
Anwar thaithita <Anwar thaithita@anwar>
2021-03-15 13:17:24 +0530

    commit to local repository

---
+++ sampletestscripts/src/test/java/sampletestscripts/test1.java
@@ -0,0 +1,20 @@
+package sampletestscripts;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public  void createcustomer() {
+		System.out.println("execute createcustomer");
+	}
+	@Test
+	public void modifycustomer() {
+		System.out.println("execute createcustomer");
+	}
+	
+	@Test
+	public void deletecustomer() {
+		System.out.println("execute createcustomer");
+	}
+
+}
